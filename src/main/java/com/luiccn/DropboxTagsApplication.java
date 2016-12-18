addc7893f10ff3761f7305c99b53f0176e17c991
Luiz Ciafrino <luiz.ciafrino@gmail.com>
2016-12-18 15:06:52 -0200

    no auth if already have token

--- src/main/java/com/luiccn/DropboxTagsApplication.java
+++ src/main/java/com/luiccn/DropboxTagsApplication.java
@@ -19,42 +19,44 @@ public class DropboxTagsApplication {
 
     public static void main(String[] args) throws IOException, DbxException {
 
+        // Open properties, we are using it to save the app token
         FileInputStream in = new FileInputStream("application.properties");
         Properties props = new Properties();
         props.load(in);
         in.close();
 
-        DbxRequestConfig requestConfig = new DbxRequestConfig(props.getProperty("dropbox.appname"));
-        //DbxAppInfo appInfo = new DbxAppInfo("i659xd69chxiq39", "4g1pyg395bqvick");
-        DbxAppInfo appInfo = new DbxAppInfo(args[0], args[1]);
-        DbxWebAuth auth = new DbxWebAuth(requestConfig, appInfo);
+        if (props.getProperty("dropbox.token").isEmpty()) {
+            DbxRequestConfig requestConfig = new DbxRequestConfig(props.getProperty("dropbox.appname"));
+            DbxAppInfo appInfo = new DbxAppInfo(args[0], args[1]);
+            DbxWebAuth auth = new DbxWebAuth(requestConfig, appInfo);
+            DbxWebAuth.Request authRequest = DbxWebAuth.newRequestBuilder()
+                    .withNoRedirect()
+                    .build();
 
-        DbxWebAuth.Request authRequest = DbxWebAuth.newRequestBuilder()
-                .withNoRedirect()
-                .build();
-        String authorizeUrl = auth.authorize(authRequest);
-        System.out.println("1. Go to " + authorizeUrl);
-        System.out.println("2. Click \"Allow\" (you might have to log in first).");
-        System.out.println("3. Copy the authorization code.");
-        System.out.print("Enter the authorization code here: ");
+            String authorizeUrl = auth.authorize(authRequest);
+            System.out.println("1. Go to " + authorizeUrl);
+            System.out.println("2. Click \"Allow\" (you might have to log in first).");
+            System.out.println("3. Copy the authorization code.");
+            System.out.print("Enter the authorization code here: ");
 
-        Scanner scan = new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
+            String code = scan.nextLine();
 
-        String code = scan.nextLine();
-        if (code != null) {
-            code = code.trim();
-            DbxAuthFinish authFinish = auth.finishFromCode(code);
-            String accessToken = authFinish.getAccessToken();
+            if (code != null) {
+                code = code.trim();
+                DbxAuthFinish authFinish = auth.finishFromCode(code);
+                String accessToken = authFinish.getAccessToken();
 
-            System.out.println("Access token:");
-            System.out.println(accessToken);
+                System.out.println("Access token:");
+                System.out.println(accessToken);
 
-            FileOutputStream out = new FileOutputStream("application.properties");
-            props.setProperty("dropbox.token", accessToken);
-            props.store(out, null);
-            out.close();
-
-            SpringApplication.run(DropboxTagsApplication.class, args);
+                FileOutputStream out = new FileOutputStream("application.properties");
+                props.setProperty("dropbox.token", accessToken);
+                props.store(out, null);
+                out.close();
+            }
         }
+
+        SpringApplication.run(DropboxTagsApplication.class, args);
     }
 }
